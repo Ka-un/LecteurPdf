d41c3b9bc8c528d86fb9f7d43f62596ac40e1e3a
K_1 <grasland.seb@gmail.com>
2023-08-07 18:03:38 +0200

    Class Upload
    création première def pour lire et assembler les pages du pdf

--- pdf.py
+++ pdf.py
@@ -1,5 +1,32 @@
 import PyPDF2
+from PyPDF2 import PdfWriter
+import re
 
-path="D:\Dev\Documents\ReleveJuillet2023"
+path="D:\Dev\Documents\ReleveJuillet2023.pdf"
 
-print('u')
+class Upload:
+
+    def Reade () -> str:
+
+        reader = PyPDF2.PdfReader(input("Taper l'emplacement du pdf"))
+        NbrPages = len(reader.pages)
+        for i in range(NbrPages-1):
+            page = reader.pages[i]
+            page_content = page.extract_text()        
+        return page_content
+
+
+reader = PyPDF2.PdfReader(input("Taper l'emplacement du pdf"))
+NbrPages = len(reader.pages)
+merger = PdfWriter()
+print(merger.append(fileobj=reader, pages=(0, NbrPages-1)))
+#for i in range(NbrPages-1):
+ #   page = reader.pages[i]
+  #  page_content = page.extract_text()
+ #   print(page_content)
+#print(page_content)
+#reader = PyPDF2.PdfReader(input("Taper l'emplacement du pdf"))
+#NbrPages = len(reader.pages)
+#writer.append
+#print(page)
+#print(Upload.Reade().extract_text())
